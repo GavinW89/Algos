f51fd8b3e8b508717870fad21a52f7b9ecfa66ce
Gavin Woods <gav5400@gmail.com>
2023-07-17 06:32:45 -0700

    Trimmed Down MergeStrings.js

--- LeetCode/MergeStrings.js
+++ LeetCode/MergeStrings.js
@@ -5,30 +5,10 @@
  */
 var mergeAlternately = function(word1, word2) {
     mergedString = '';
-    if(word1.length == word2.length){
-        for (i=0; i < word1.length; i++){
-            mergedString += word1[i];
-            mergedString += word2[i];
-        }
-    }else{
-        if(word1.length > word2.length){
-            for(i=0; i < word1.length; i++){
-                mergedString += word1[i]
-                if(word2[i]){
-                    mergedString += word2[i]
-                }
-            }
-        }else{
-            for(i=0; i < word2.length; i++){
-                if(word1[i]){
-                    mergedString += word1[i]
-                }
-                mergedString += word2[i]
-            }
-        }
+    for(let i = 0; i < Math.max(word1.length, word2.length); i++){
+        if(i < word1.length) mergedString += word1[i];
+        if(i < word2.length) mergedString += word2[i];
     }
         return mergedString
 };
-
-
 console.log(mergeAlternately("bark", "to"));
